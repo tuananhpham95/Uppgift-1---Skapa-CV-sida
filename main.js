a9beed186ad45bee2a8d282d70b35050eac94605
Tuan Anh Pham <tuananh@hotmail.se>
2022-12-19 06:52:40 +0100

    fixat små del

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const navIcons = document.querySelector('.navbar__icons');
 const navLinks = document.querySelector('.navbar__links');
-const banner = document.querySelector('.banner')
 
 navIcons.addEventListener('click',()=>{
     navLinks.classList.toggle('navbar__open');
